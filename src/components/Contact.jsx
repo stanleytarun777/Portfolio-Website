9cc9378bc9112e5d54a27a75f6bab2eeb71cc134
Stanley N <stanleytarun777@gmail.com>
2026-02-01 16:30:35 -0600

    Prevent skill card overflow

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
             <h2>Contact</h2>
             <p>Email: {" "}
                 <a href="mailto:snyford@nicholls.edu">snyford@nicholls.edu </a>
-                Phone: {"  "}
-                <a href="tel:+13056462288">(985) 426-2099</a>
+                Phone: {" "}
+                <a href="tel:+13056462288"> (985) 426-2099</a>
             </p>
         </section>
     );
