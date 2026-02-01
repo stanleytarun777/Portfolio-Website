b56d94c49e6d41b309a5d905f573a6f0e45bf8fb
Stanley N <stanleytarun777@gmail.com>
2026-02-01 16:40:21 -0600

    Improve navbar list spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="footer">
-        <p>© {new Date().getFullYear()} Stanley NFORD</p>
+        <p>© {new Date().getFullYear()} Stanley NYFORD</p>
         <p>Built with React | Designed for performance and Clarity.</p>
         </footer>
     );
